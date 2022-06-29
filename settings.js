0c72e833258b5e1c5d7fa353b6178d572ecde089
Wync <Tootie-@live.com.sg>
2022-06-30 00:52:19 +0800

    .

--- settings.js
+++ settings.js
@@ -1,25 +1,25 @@
 const collectionInfo = {
-    name: "VILLINZ",
+    name: "Future Wars",
     title: "{name}",
-    date: "27.06.2022",
+    date: "29.06.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "logo.jpg",
+        preview: "1.gif",
         favicon: "logo.jpg",
     },
     background: {
         type: "image",
-        image: "bg.jpg",
+        image: "bg.png",
         video: "",
         color: "#4E4E6D",
     }
 }
 const mintInfo = {
-    price: 1,
-    totalSupply: 4888,
+    price: 2,
+    totalSupply: 4777,
     toFixed: 1,
     minUnits: 1,
     maxUnits: 3,
@@ -29,7 +29,7 @@ const mintInfo = {
 const nftsInfo = {
     active: true,
     minValue: 0.3,
-    maxTransfer: 3,
+    maxTransfer: 5,
 }
 
 if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
